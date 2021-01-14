362d8392199f24eefc7a8c40769b93d26808a7f4
Magali Rézeau <magalirezeau@gmail.com>
2021-01-14 19:48:25 +0100

    header and section-about added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
+    <app-main></app-main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    AppMain,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+:root {
+  $fs-xs: .75rem;
+  $fs-s: .8125rem;
+  $fs-m: .9375rem;
+  $fs-l: 1.125rem;
+  $fs-xl: 2rem;
+  $fw-regular: 400;
+  $fw-bold: 700;
 }
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+  font-size: 12px;
+  font-family: 'DM Sans', sans-serif;
+}
+ul {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: black;
+  text-transform: uppercase;
+}
+.btn {
+  background-color: black;
+  color: white;
+  width: 19.375rem;
+  height: 3rem;
+  text-transform: uppercase;
+}
+figure {
+  width: 100%;
+  height: 100%;
+}
+img {
+  display: block;
+   width: 100%;
+  height: 100%;
+}
+button {
+  border: none;
+  background-color: transparent;
+}
+
 </style>
